f5a3d73728fdee7ae21b62e0180f53866f2f3ddb
Aurora-is-asleep <azurathantthiri@gmail.com>
2025-05-11 14:28:23 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    initCharts();
+});
+const energyCtx = document.getElementById('energyChart');
+if (energyCtx) {
+    new Chart(energyCtx, {
+        // chart config
+    });
+}
